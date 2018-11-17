76a15fd5032386c03bf99b4cd5c321444270a333
antara <antarasen95@gmail.com>
2018-11-17 19:18:52 +0530

    added like unlike feature

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { deletePost, addLike, removeLike } from '../../actions/postActions';
 
 class PostItem extends Component{
+
+    onDeleteClick(id) {
+      this.props.deletePost(id);
+
+    }
+
+    onLikeClick(id) {
+      this.props.addLike(id);
+    }
+
+    onUnlikeClick(id){
+      this.props.removeLike(id)
+    }
+
     render() {
         const { post, auth } = this.props;
         return (
@@ -15,11 +30,15 @@ class PostItem extends Component{
           </div>
           <div className="col-md-10">
             <p className="lead">{post.text}</p>
-                <button type="button" className="btn btn-light mr-1">
+                <button type="button" 
+                  onClick={this.onLikeClick.bind(this, post._id)}
+                  className="btn btn-light mr-1">
                   <i className="text-info fas fa-thumbs-up" />
                   <span className="badge badge-light">{post.likes.length}</span>
                 </button>
-                <button type="button" className="btn btn-light mr-1">
+                <button type="button" 
+                  onClick={this.onUnlikeClick.bind(this, post._id)}
+                  className="btn btn-light mr-1">
                   <i className="text-secondary fas fa-thumbs-down" />
                 </button>
                 <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
@@ -29,6 +48,7 @@ class PostItem extends Component{
                   <button
                     type="button"
                     className="btn btn-danger mr-1"
+                    onClick={this.onDeleteClick.bind(this, post._id)}
                   >
                     <i className="fas fa-times" />
                   </button>
@@ -41,6 +61,9 @@ class PostItem extends Component{
 }
 
 PostItem.propTypes = {
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired,
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired
 }
@@ -49,4 +72,4 @@ const mapStateToProps = state => ({
     auth: state.auth
 })
 
-export default connect(mapStateToProps)(PostItem);
+export default connect(mapStateToProps, { deletePost, addLike, removeLike })(PostItem);
